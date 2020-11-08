c1cd3cca5c10b324e62244b5d5dea6c7085f99ba
wangzhi <15713598138@sina.cn>
2020-11-08 21:16:31 +0800

    优化异步获取子线程执行的结果

--- Week_04/multithreading/src/main/java/com/wangzhi/thread/homework/CallableDemo.java
+++ Week_04/multithreading/src/main/java/com/wangzhi/thread/homework/CallableDemo.java
@@ -12,13 +12,35 @@ public class CallableDemo implements Callable<Integer> {
 
     @Override
     public Integer call() throws Exception {
-        return 1;
+        Thread.sleep(2000);
+        return sum();
     }
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         FutureTask<Integer> integerFutureTask = new FutureTask<>(new CallableDemo());
         new Thread(integerFutureTask).start();
+        System.out.println("主线程继续执行");
         Integer integer = integerFutureTask.get();
-        System.out.println(integer);
+        System.out.println("异步获取到的结果：" + integer);
+    }
+
+    private static int sum() {
+        return fibo(36);
+    }
+
+    private static int fibo(int a) {
+        if (a < 2) {
+            return 1;
+        }
+        int first = 1;
+        int right = 1;
+        for (int i = 2; i <= a; i++) {
+            int temp = right;
+            right = first + right;
+            first = temp;
+        }
+        return right;
     }
 }
+
+
